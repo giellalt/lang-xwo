aa05f29b149df61cfe064c84e3a087af428d9987
Trond Trosterud <trond.trosterud@uit.no>
2015-04-16 10:59:44 +0000

    For reference:
    This is a perlscript converting from latin to Mongolian,
    just as the xfscript src/orthography/Latn-to-Mong.xfscript.
    
    The xfscript is integrated in the transducer infrastructure,
    and should be used together with the transducer.
    
    For simple converting of text, both the xfscript and the
    perl script may be used.

---
+++ test/data/LatinToTodo.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+use utf8 ;
+
+while (<>)
+
+{
+
+s/N G/ᡊ/g ;
+s/A/ᠠ/g ;
+s/E/ᡄ/g ;
+s/I/ᡅ/g ;
+s/%0/ᡆ/g ;
+s/V/ᡇ/g ;
+s/O/ᡈ/g ;
+s/U/ᡉ/g ;
+s/N/ᠨ/g ;
+s/B/ᡋ/g ;
+s/P/ᠫ/g ;
+s/H/ᡍ/g ;
+s/G/ᡎ/g ;
+s/M/ᠮ/g ;
+s/L/ᠯ/g ;
+s/R/ᠷ/g ;
+s/S/ᠰ/g ;
+s/%$/ᠱ/g ;
+s/D/ᡑ/g ;
+s/T/ᡐ/g ;
+s/C/ᡒ/g ;
+s/c/ᡔ/g ;
+s/Z/ᡓ/g ;
+s/j/ᡚ/g ;
+s/%:/ᡃ/g ;
+s/%,/᠂/g ;
+s/%./᠃/g  ;
+
+print ;
+}
+
+
+
+
+
+
+
+
+
